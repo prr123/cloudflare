a32825078a8e026af4b590874eec95542762087c
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2023-03-29 15:54:38 +0200

    added PrintResInfo, PrintDnsRec, PrintUserInfo!

--- cfLib/cfLib.go
+++ cfLib/cfLib.go
@@ -8,7 +8,9 @@ package cfLib
 import (
 	"fmt"
 	"os"
+
     yaml "github.com/goccy/go-yaml"
+    "github.com/cloudflare/cloudflare-go"
 )
 
 
@@ -21,7 +23,7 @@ type ApiObj struct {
 	YamlFile	string
 }
 
-func InitClLib(yamlFilNam string) (apiObjRef *ApiObj, err error) {
+func InitCfLib(yamlFilNam string) (apiObjRef *ApiObj, err error) {
 	var apiObj ApiObj
 
     // open file and decode
@@ -55,4 +57,60 @@ func PrintApiObj (apiObj *ApiObj) {
 //    fmt.Printf("Ca Dir Url: %s\n", nchObj.CADirUrl)
     fmt.Printf("Email:    %s\n", apiObj.Email)
     fmt.Println("*******************************")
+}
+
+// https://github.com/cloudflare/cloudflare-go/blob/0d05fc09483641dde8abb4c64cf2f6016f590d79/user.go#L12
+func PrintUserInfo (u *cloudflare.User) {
+
+    var actTyp string
+
+    fmt.Println("************** User Info **************")
+    fmt.Printf("First Name:  %s\n", u.FirstName)
+    fmt.Printf("Last Name:   %s\n", u.LastName)
+    fmt.Printf("Email:       %s\n", u.Email)
+    fmt.Printf("ID:          %s\n", u.ID)
+    fmt.Printf("Country:     %s\n", u.Country)
+    fmt.Printf("Zip Code:    %s\n", u.Zipcode)
+    fmt.Printf("Phone:       %s\n", u.Telephone)
+    fmt.Printf("2FA:         %t\n", u.TwoFA)
+    timStr := (u.CreatedOn).Format("02 Jan 06 15:04 MST")
+    fmt.Printf("Created:     %s\n", timStr)
+    timStr = (u.ModifiedOn).Format("02 Jan 06 15:04 MST")
+    fmt.Printf("Modified:    %s\n", timStr)
+    fmt.Printf("ApiKey:      %s\n", u.APIKey)
+    if len(u.Accounts) == 1 {
+        act := u.Accounts[0]
+        actTyp = act.Type
+        if len(actTyp) == 0 {actTyp = "-"}
+        fmt.Printf("account ID: %s Name: %s Type: %s\n", act.ID, act.Name, actTyp)
+    } else {
+        fmt.Printf("Accounts [%d]:\n", len(u.Accounts))
+        fmt.Printf("Nu ID  Name  Type\n")
+        for i:=0; i< len(u.Accounts); i++ {
+            act := u.Accounts[i]
+            actTyp = act.Type
+            if len(actTyp) == 0 {actTyp = "-"}
+            fmt.Printf("%d: %s %s %s\n", i+1, act.ID, act.Name, actTyp)
+        }
+    }
+    fmt.Println("********** End User Info **************")
+}
+
+func PrintResInfo(res *cloudflare.ResultInfo) {
+
+    fmt.Println("************** ResultInfo **************")
+    fmt.Printf("Page:       %d\n", res.Page)
+    fmt.Printf("PerPage:    %d\n", res.PerPage)
+    fmt.Printf("TotalPages: %d\n", res.TotalPages)
+    fmt.Printf("Count:      %d\n", res.Count)
+    fmt.Printf("Total:      %d\n", res.Total)
+    fmt.Println("********** End ResultInfo **************")
+}
+
+func PrintDnsRec(recs *[]cloudflare.DNSRecord) {
+    fmt.Printf("************* DNS Recourds: %d ************\n", len(*recs))
+    fmt.Println("number  type      name             value/ content            Id")
+    for i:=0; i< len(*recs); i++ {
+        fmt.Printf("Record[%d]: %-3s %s %s %s\n", i, (*recs)[i].Type, (*recs)[i].Name, (*recs)[i].Content, (*recs)[i].ID)
+    }
 }
