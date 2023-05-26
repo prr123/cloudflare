3f322eb01c76c9c953ba7a3410f5c0537d1a0847
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2023-05-26 18:31:40 +0200

    use new cfIniApi

--- listDnsRec.go
+++ listDnsRec.go
@@ -16,7 +16,18 @@ func main() {
 
     numArgs := len(os.Args)
 	useStr :=  "usage: listDnsRec domain [/yaml=file]\n"
-    DomainFilNam := "cfDomainsShort.yaml"
+
+	zoneDir := os.Getenv("zoneDir")
+	if len(zoneDir) == 0 {log.Fatalf("could not resolve env var zoneDir!")}
+
+    DomainFilNam := zoneDir + "/cfDomainsShort.yaml"
+
+	cfDir := os.Getenv("Cloudflare")
+	if len(cfDir) == 0 {log.Fatalf("could not resolve env var Cloudflare!")}
+
+    yamlApiFilNam := cfDir + "/token/cfDnsApi.yaml"
+
+	dbg:= true
 
     if numArgs < 2 {
         fmt.Printf(useStr)
@@ -27,7 +38,7 @@ func main() {
         log.Fatalf("too many CLI args!\n")
     }
 
-    yamlFilNam := "cloudflareApi.yaml"
+//    yamlFilNam := "cloudflareApi.yaml"
 	domain := os.Args[1]
 
 	if len(domain) == 0 {
@@ -51,57 +62,45 @@ func main() {
 		if !ok {
 			log.Fatalf("no yaml file specified!")
 		}
-		yamlFilNam, ok = val.(string)
+		yamlApiFilNam, ok = val.(string)
 		if !ok {
 			log.Fatalf("no yaml file value not a string!")
 		}
 	}
 
 	log.Printf("Lookup DNS Records for domain: %s\n", domain)
-    log.Printf("Using yaml file: %s\n", yamlFilNam)
-    log.Printf("Using yaml domainsfile: %s\n", DomainFilNam)
+    log.Printf("Using token file: %s\n", yamlApiFilNam)
+    log.Printf("Using domain file: %s\n", DomainFilNam)
 
-    apiObj, err := cfLib.InitCfLib(yamlFilNam)
-    if err != nil {
-        log.Fatalf("cfLib.InitCfLib: %v\n", err)
-    }
+    apiObj, err := cfLib.InitCfApi(yamlApiFilNam)
+    if err != nil {log.Fatalf("cfLib.InitCfLib: %v\n", err)}
+
     // print results
-    cfLib.PrintApiObj (apiObj)
+    cfLib.PrintApiObj (apiObj.ApiObj)
 
-	// Construct a new API object using a global API key
-//	api, err := cloudflare.New(os.Getenv("CLOUDFLARE_API_KEY"), os.Getenv("CLOUDFLARE_API_EMAIL"))
-	// alternatively, you can use a scoped API token
-
-	api, err := cloudflare.NewWithAPIToken(apiObj.ApiToken)
-	if err != nil {
-		log.Fatalf("api init: %v/n", err)
-	}
 
 	// Most API calls require a Context
 	ctx := context.Background()
+	api := apiObj.API
 
 	fmt.Println("********************************************")
 
 	//get Domain id file
+    zoneList, err := cfLib.ReadZoneShortFile(DomainFilNam)
+    if err != nil {log.Fatalf("ReadZoneFileShort: %v\n", err)}
+
+    log.Printf("success reading all cf zones!\n")
+    if dbg {cfLib.PrintZoneList(zoneList)}
+
+    numZones := len(zoneList.Zones)
     if _, err := os.Stat(DomainFilNam); err != nil {
         log.Fatalf("no existing domain file: %v!\n", err)
     }
-
-    infil, err := os.Open(DomainFilNam)
-    if err != nil {
-        log.Fatal("could not open file %s: %v", DomainFilNam, err)
-    }
-
-    var zoneShortList *[]cfLib.ZoneShort
-
-	zoneShortList, err = cfLib.ReadZonesShortYaml(infil)
-	if err != nil {
-		log.Fatalf("cfLib.ReadZonesShortYaml: %v\n", err)
-	}
+	log.Printf("Zones: %d\n",numZones)
 
 	found := -1
-    for i:=0; i<len((*zoneShortList)); i++ {
-        zone := (*zoneShortList)[i]
+    for i:=0; i<numZones; i++ {
+        zone := zoneList.Zones[i]
 		if zone.Name == domain {
 			found = i
 			break
@@ -111,7 +110,7 @@ func main() {
 		log.Fatalf("domain: %s not found!\n", domain)
 	}
 
-	zone := (*zoneShortList)[found]
+	zone := zoneList.Zones[found]
 	fmt.Printf("Zone[%d]: Name: %s Id: %s\n", found+1, zone.Name, zone.Id)
 
 	// try to list DNS Parameters
@@ -119,18 +118,11 @@ func main() {
 
 	var rc cloudflare.ResourceContainer
 	rc.Level = cloudflare.ZoneRouteLevel
-//	rc.Identifier = "0e6e30d5edb4c1025817eb1678511cef"
 	rc.Identifier = zone.Id
 
-//	dnsRecs, resInfo, err := api.ListDNSRecords(ctx, &rc, listDns)
 	dnsRecs, _, err := api.ListDNSRecords(ctx, &rc, listDns)
     if err != nil {
         log.Fatalf("api.ListDNSRecords: %v\n", err)
     }
-//	fmt.Printf("resInfo: %v\n", resInfo)
-//	fmt.Printf("Dns Records [%d]\n",len(dnsRecs))
-
-//	cfLib.PrintResInfo(resInfo)
 	cfLib.PrintDnsRecs(&dnsRecs)
 }
-
